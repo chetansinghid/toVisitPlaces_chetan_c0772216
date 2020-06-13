e4f70d1a14b44770906c124de16cd086292cd887
Chetan Singh <chetan.singh.0602@gmail.com>
2020-06-13 15:30:23 -0400

    Added user location display

--- Find My Place/ViewController.swift
+++ Find My Place/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+//reference to storyboard mapview
+    @IBOutlet weak var mapView: MKMapView!
+    
+//    location manager declaration
+    var locationManager = CLLocationManager()
+    
+//    on load function
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+//        sets up location manager
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
     }
 
+//    MARK: updates location in map when user moves - displays current location
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        delta region specifiers
+        let latDelta: CLLocationDegrees = 0.05
+        let lngDelta: CLLocationDegrees = 0.05
+        
+//        create region to show to user
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lngDelta)
+        let region = MKCoordinateRegion(center: locations.last!.coordinate, span: span)
+        
+//        displays region
+        mapView.setRegion(region, animated: true)
+    }
 
 }
 
